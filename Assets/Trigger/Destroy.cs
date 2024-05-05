f16a9230a3d2b040ab271d9aebe29a66bfca4e19
sayIshuu <smile52673@gmail.com>
2024-05-06 05:01:36 +0900

    초록도시 세팅완료

--- Assets/Trigger/Destroy.cs
+++ Assets/Trigger/Destroy.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Destroy : MonoBehaviour
 {
+    public Transform mainSpere;
+    private float mainSpereYRotation = 0.0f;
+
     // 이 함수는 다른 오브젝트의 Collider가 이 오브젝트의 Trigger에 들어올 때 자동으로 호출됩니다.
     private void OnTriggerEnter(Collider other)
     {
@@ -12,6 +15,9 @@ public class Destroy : MonoBehaviour
             // 'other'는 트리거 영역에 들어온 오브젝트의 Collider입니다.
             // 여기서는 단순히 사라지게 하기 위해 Destroy 함수를 사용합니다.
             Destroy(other.gameObject);
+
+            mainSpereYRotation += 4.0f;
+            mainSpere.Rotate(0.0f, mainSpereYRotation, 0.0f);
         }
     }
 }
---
+++ Assets/Trigger/OnlyLightChange.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnlyLightChange : MonoBehaviour
+{
+    public float changeSpeed = 0.05f; // 색상 변화에 걸리는 시간 (초)
+
+    public Light light;
+
+    private bool isColorChanging = false;
+    private bool isColorReturning = false;
+
+    Color lightColor;
+
+    Color targetLightColor;
+
+    void Start()
+    {
+        lightColor = light.color;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("P1") || other.CompareTag("P2"))
+        {
+            GameObject rere = other.gameObject;
+            MeshRenderer meshRenderer = rere.GetComponent<MeshRenderer>();
+            if (meshRenderer == null)
+            {
+                meshRenderer = rere.AddComponent<MeshRenderer>();
+            }
+
+            Transform waterTransform = rere.transform.Find("Sphere");
+
+            Renderer waterRenderer = waterTransform.GetComponent<SkinnedMeshRenderer>();
+
+            targetLightColor = waterRenderer.material.GetColor("_TopColor");
+
+            isColorChanging = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("P1") || other.CompareTag("P2"))
+        {
+            targetLightColor = lightColor;
+
+            isColorReturning = true;
+        }
+    }
+
+
+    private void Update()
+    {
+        if (isColorChanging)
+        {
+            // 보간하여 색상을 부드럽게 변경
+            light.color = Color.Lerp(light.color, targetLightColor, changeSpeed * Time.deltaTime);
+
+            // 목표 색에 근접하면 색 변화 종료
+            if (Vector4.Distance(light.color, targetLightColor) < 0.01f)
+            {
+                light.color = targetLightColor; // 목표 색으로 설정
+                isColorChanging = false; // 색 변화 종료
+            }
+        }
+        if (isColorReturning)
+        {
+            // 보간하여 색상을 부드럽게 변경
+            light.color = Color.Lerp(light.color, targetLightColor, changeSpeed * Time.deltaTime);
+
+            // 목표 색에 근접하면 색 변화 종료
+            if (Vector4.Distance(light.color, targetLightColor) < 0.01f)
+            {
+                light.color = targetLightColor; // 목표 색으로 설정
+                isColorReturning = false; // 색 변화 종료
+            }
+        }
+    }
+}
