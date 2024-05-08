6b6f33f15ebece81568f5136983cd5ab50279927
sayIshuu <smile52673@gmail.com>
2024-05-09 04:10:47 +0900

    0509 전체씬 보정 및 파랑도시 발전 내일은 파란도시 확정, 보정도 마무리

--- Assets/Scripts/FogController.cs
+++ Assets/Scripts/FogController.cs
@@ -6,10 +6,14 @@ public class FogController : MonoBehaviour
 {
     public Color grayFogColor;
     public float grayFogDensity;
+    public Material graySky;
 
     public Color greenFogColor;
     public float greenFogDensity;
 
+    public Color blueFogColor;
+    public float blueFogDensity;
+    public Material blueSky;
     void Start()
     {
         RenderSettings.fogColor = grayFogColor; // 안개 색상 설정
@@ -20,10 +24,18 @@ public class FogController : MonoBehaviour
     {
         RenderSettings.fogColor = grayFogColor;
         RenderSettings.fogDensity = grayFogDensity;
+        RenderSettings.skybox = graySky;
     }
     public void ToGreen()
     {
         RenderSettings.fogColor = greenFogColor;
         RenderSettings.fogDensity = greenFogDensity;
+        RenderSettings.skybox = graySky;
+    }
+    public void ToBlue()
+    {
+        RenderSettings.fogColor = blueFogColor;
+        RenderSettings.fogDensity = blueFogDensity;
+        RenderSettings.skybox = blueSky;
     }
 }
